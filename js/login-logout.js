c13f78fe91bbb11bbfa48fbe83ff0a749438e80e
Theanordhoy <thea.n.k@hotmail.com>
2025-12-01 11:42:02 +0100

    added login and logout button with functionality

---
+++ js/login-logout.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("loginButton");
+
+function isUserLoggedIn() {
+    const token = localStorage.getItem("accessToken");
+    return token;
+}
+
+button.addEventListener("click", function() {
+    if (!isUserLoggedIn()) {
+        window.location.href = "../html/account/login.html";
+    } else {
+        localStorage.removeItem("accessToken");
+        window.location.href = "/index.html";
+    }
+})
+
+function updateButton() {
+    if (isUserLoggedIn()) {
+        button.textContent = "Logout";
+    } else {
+        button.textContent = "Login";
+    }
+}
+
+updateButton()
